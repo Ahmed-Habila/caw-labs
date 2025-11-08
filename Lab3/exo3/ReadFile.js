8d9446fc4abbafa6dff15c09a6b2a5b3a96fb9d9
chakib <genchakib@gmail.com>
2025-11-08 14:02:28 +0100

    Lab3-final

--- Lab3/exo3/ReadFile.js
+++ Lab3/exo3/ReadFile.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
 
 
-const data = fs.readFileSync('./newtxt.txt', 'utf8');
+const data = fs.readFileSync('./filer.txt', 'utf8');
 console.log(data);
--- Lab3/exo4/exo4.js
+++ Lab3/exo4/exo4.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
 const text = process.argv[2];
-fs.writeFileSync('./filer.txt',text)
+const text2 = process.argv[3];
+fs.writeFileSync(text,text2)
 console.log('the file has been saved');
